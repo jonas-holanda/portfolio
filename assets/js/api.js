998af840f456faca6bafae6007f3e772fb47197d
Jonas Holanda <jonasholanda15@gmail.com>
2024-01-25 22:56:51 -0300

    fix: alterado url da requisição da api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fechProfileData() {
-    const url = `../../data/profile.json`;
+    const url = `https://raw.githubusercontent.com/jonas-holanda/portfolio/main/data/profile.json`;
     const fetching = await fetch(url);
     return await fetching.json();
 }
